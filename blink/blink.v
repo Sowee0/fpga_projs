01f22979ada48639b8ec20e35bec29b2cd189faa
Gabriel Oliveira <sowee00@gmail.com>
2022-05-23 20:46:13 -0300

    Adding a counter to the blink code for more leds

--- blink/blink.v
+++ blink/blink.v
@@ -1,17 +1,20 @@
 module blink (
 clk,
-out
+leds,
 );
 
-output reg out;
+
 
 input clk;
+
+output reg [3:0] leds;
 
 reg [31:0] counter;
 
 initial begin
     counter <= 32'b0;
-    out <= 1'b0;
+
+	 leds <= 4'b0;
 
 end
 
@@ -20,7 +23,7 @@ always @(posedge clk) begin
     counter <= counter + 1'b1;
 
     if (counter > 50000000) begin
-        out <= !out;
+        leds = leds + 1'b1;
         counter <= 32'b0;
     end
 
